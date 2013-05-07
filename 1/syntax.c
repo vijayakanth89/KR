59720b1341fb9a48ca51d035e171bfeea97880ff
Vijayakanth <vijayakanth@vijayakanth-HP430.(none)>
2013-05-08 01:25:37 +0530

    change in syntax checker

--- 1/syntax.c
+++ 1/syntax.c
@@ -6,13 +6,21 @@ char SQUARE_BRACE[] = {'[',']'};
 char DOUBLE_QUOTES[] = {'\"','\"'};
 char SINGLE_QUOTES[] = {'\'', '\''};
 
+int START = 0;
+int END = 1;
+
+
 
 int main(int argc, char *argv[])
 {
 	char ch;
 	while( (ch = getchar() ) != EOF){
-		if (ch == '{' || ch == '}')
-			printf("%c", ch);	
+		switch(ch){
+			case DOUBLE_BRACE[START]:
+				// register in map
+				break;
+		}
+	
 	}
 	return 0;
 }
